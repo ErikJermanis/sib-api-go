963047a92ff372ac3115a832b1aef1fc06820799
ErikJermanis <erik55jermanis@gmail.com>
2024-04-07 15:38:52 +0200

    Added authentication and protected all routes.

--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
@@ -30,6 +32,10 @@ type RecordBody struct {
 	Text string
 }
 
+type AuthenticateBody struct {
+	Otp string
+}
+
 type responseJson map[string]string
 
 func (app *App) Initialize(user, password, dbname string) {
@@ -46,11 +52,12 @@ func (app *App) Initialize(user, password, dbname string) {
 }
 
 func (app *App) setRouters() {
-	app.Get("/records", app.getRecords)
-	app.Get("/records/{id}", app.getRecord)
-	app.Post("/records", app.createRecord)
-	app.Put("/records/{id}", app.updateRecord)
-	app.Delete("/records/{id}", app.deleteRecord)
+	app.Post("/authenticate", app.authenticate)
+	app.Get("/records", app.protected(app.getRecords))
+	app.Get("/records/{id}", app.protected(app.getRecord))
+	app.Post("/records", app.protected(app.createRecord))
+	app.Put("/records/{id}", app.protected(app.updateRecord))
+	app.Delete("/records/{id}", app.protected(app.deleteRecord))
 }
 
 func (app *App) Get(path string, handler func(writer http.ResponseWriter, request *http.Request)) {
@@ -67,6 +74,87 @@ func (app *App) Put(path string, handler func(writer http.ResponseWriter, reques
 
 func (app *App) Delete(path string, handler func(writer http.ResponseWriter, request *http.Request)) {
 	app.Router.HandleFunc(path, handler).Methods("DELETE")
+}
+
+func (app *App) protected(handler http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		authHeader := request.Header.Get("Authorization")
+		if authHeader == "" {
+			respondWithJSON(writer, http.StatusUnauthorized, responseJson{ "message": "Access denied." })
+			return
+		}
+		
+		bearerToken := strings.Split(authHeader, " ")
+		if len(bearerToken) != 2 {
+			respondWithJSON(writer, http.StatusUnauthorized, responseJson{ "message": "Access denied." })
+			return
+		}
+
+		token, err := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte(os.Getenv("SIB_API_JWT_SECRET")), nil
+		})
+
+		if err != nil {
+			respondWithJSON(writer, http.StatusUnauthorized, responseJson{ "message": "Access denied." })
+			return
+		}
+
+		if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			handler.ServeHTTP(writer, request)
+		} else {
+			respondWithJSON(writer, http.StatusUnauthorized, responseJson{ "message": "Access denied." })
+			return
+		}
+	}
+}
+
+func (app *App) authenticate(writer http.ResponseWriter, request *http.Request) {
+	var requestBody AuthenticateBody;
+	if err := json.NewDecoder(request.Body).Decode(&requestBody); err != nil {
+		respondWithJSON(writer, http.StatusBadRequest, responseJson{ "message": err.Error() })
+		return
+	}
+
+	if requestBody.Otp == "" {
+		respondWithJSON(writer, http.StatusBadRequest, responseJson{ "message": "'otp' field is required" })
+		return
+	}
+	var used bool;
+	var expiresAt time.Time
+	err := app.DB.QueryRow("SELECT used, expiresat FROM otps WHERE otp = $1", requestBody.Otp).Scan(&used, &expiresAt)
+	if err != nil || used || time.Now().After(expiresAt) {
+		respondWithJSON(writer, http.StatusUnauthorized, responseJson{ "message": "Access denied." })
+		return
+	}
+	// if err != nil {
+	// 	respondWithJSON(writer, http.StatusUnauthorized, responseJson{ "message": "Access denied, row not found." })
+	// 	return
+	// }
+	// if used {
+	// 	respondWithJSON(writer, http.StatusUnauthorized, responseJson{ "message": "Access denied, already used." })
+	// 	return
+	// }
+	// if time.Now().After(expiresAt) {
+	// 	respondWithJSON(writer, http.StatusUnauthorized, responseJson{ "message": "Access denied, expired" })
+	// 	return
+	// }
+	
+	_, err = app.DB.Exec("UPDATE otps SET used = true WHERE otp = $1", requestBody.Otp)
+	if err != nil {
+		respondWithJSON(writer, http.StatusInternalServerError, responseJson{ "message": err.Error() })
+		return
+	}
+
+	token, err := generateJWT()
+	if err != nil {
+		respondWithJSON(writer, http.StatusInternalServerError, responseJson{ "message": err.Error() })
+		return
+	}
+
+	respondWithJSON(writer, http.StatusOK, responseJson{ "token": token })
 }
 
 func (app *App) getRecords(writer http.ResponseWriter, request *http.Request) {
@@ -199,6 +287,17 @@ func (app *App) deleteRecord(writer http.ResponseWriter, request *http.Request) 
 	}
 
 	respondWithJSON(writer, http.StatusOK, responseJson{ "message": "record deleted successfully." })
+}
+
+func generateJWT() (string, error) {
+	claims := jwt.RegisteredClaims{
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24 * 365 * 100)),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("SIB_API_JWT_SECRET")))
+	return tokenString, err
 }
 
 func respondWithJSON(writer http.ResponseWriter, statusCode int, payload interface{}) {
